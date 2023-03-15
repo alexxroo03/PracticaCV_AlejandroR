8f15969c3e8725c6a3ece8aab2bc9e826fe5e134
User_ <User_@APPEXPERT040320>
2023-03-15 17:28:42 +0100

    Agregar archivos de proyecto.

---
+++ PracticaCV_AlejandroRR/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace PracticaCV_AlejandroRR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //CONSTANTES
+            //VARIABLES
+            //INICIALIZACION
+            //ENTRADA
+            //PORCESAMIENTO
+            //SALIDA
+        }
+    }
+}
